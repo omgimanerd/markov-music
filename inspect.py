8efdb94f4857a6ce712976bf22f482ea5aa2619a
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-09 16:47:20 -0500

    added inspect.py to dissect a midi

---
+++ inspect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import mido
+
+def inspect(filename):
+    mid = mido.MidiFile(filename)
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        for message in track:
+            print(message)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2:
+        inspect(sys.argv[1])
--- test.py
+++ test.py
@@ -6,9 +6,6 @@ mid = mido.MidiFile("midi/river_flows.mid")
 
 print mid
 
-track1 = None
-track2 = None
-
 for i, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(i, track.name))
     for message in track[:75]:
@@ -33,10 +30,6 @@ with mido.midifiles.MidiFile() as midi:
     add(64, 100, 1000)
     add(64, 100, 1000)
     add(64, 0, 1000)
-    # add(81, 71, 0)
-    # add(54, 72, 1000)
-    # add(81, 0, 1000)
-    # add(54, 0, 0)
 
     midi.tracks.append(track)
     midi.save('new_song.mid')
